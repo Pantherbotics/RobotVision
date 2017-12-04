417f8d43e2109be2e7a97c981a24369c9726a366
Aaron Fang <aaron45765@gmail.com>
2017-12-03 18:13:20 -0800

    Move Camera Access to main

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-import streamviewer
+import cv2, time
 from grip import GripPipeline
 
-pipeline = GripPipeline
+pipeline = GripPipeline()
+stream = None
+url = 'http://127.0.0.1:1180/?action=stream'
 while(True):
-    pipeline.process(streamviewer.getframe())
+    if not stream: stream = cv2.VideoCapture(url)
+    connected, frame = stream.read()
+    if not connected:
+        print('RIP Camera')
+        time.sleep(1)
+        stream.release()
+        stream = None
+        continue
+    pipeline.process(frame)
+
 
